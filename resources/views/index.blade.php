1fc950d55a3c3f8c6ffebb81891d80e597f5dc3b
mark <markpackham1@gmail.com>
2021-12-09 15:19:14 +0000

    add Convert header
    https://youtu.be/dHPcWbC62R4?t=1121

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,1 +1,21 @@
-TEST
+@extends('layouts.app')
+
+@section('content')
+
+    <div class="min-w-screen min-h-screen bg-gray-100 flex flex-col items-center justify-center">
+        <div class="w-5/6 lg:w-3/6 rounded-xl bg-gradient-to-b shadow-xl">
+            <div class="text-white py-4 bg-gray-200">
+                <div class="text-center font-bold text-2xl text-blue-600">
+                    <h2>
+                        <i class="fab fa-gg"></i>
+                        Convert
+                    </h2>
+                </div>
+                <form action="/convert" method="POST">
+                    @csrf
+                </form>
+            </div>
+        </div>
+    </div>
+
+@endsection
